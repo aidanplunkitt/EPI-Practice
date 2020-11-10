6ea5df861b81f155dfb142d83f9d23ca8c115036
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-11-10 13:35:35 -0500

    Solution to 5.6 buy and sell stock once

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -4,8 +4,14 @@ from test_framework import generic_test
 
 
 def buy_and_sell_stock_once(prices: List[float]) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    max_net = 0.0
+    min_price = prices[0]
+
+    for i in range(len(prices)):
+        min_price = min(min_price, prices[i])
+        max_net = max(max_net, prices[i] - min_price)
+
+    return max_net
 
 
 if __name__ == '__main__':
