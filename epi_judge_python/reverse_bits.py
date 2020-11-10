fc317b72b9762f4f5cbe7078386182cefb9993f0
Aidan Plunkitt <aidan@Aidans-MBP.hsd1.fl.comcast.net>
2020-11-10 09:58:41 -0500

    Initial solution to 4.3 reverse_bits (p. 31)

--- epi_judge_python/reverse_bits.py
+++ epi_judge_python/reverse_bits.py
@@ -1,9 +1,25 @@
 from test_framework import generic_test
 
+def reverse_16(x: int) -> int:
+    result = 0
+    for _ in range(16):
+        result = (result << 1) + (x & 1)
+        x >>= 1
+
+    return result
+
+LUT_16 = [reverse_16(x) for x in range(2 ** 16)]
+MASK = 0xFFFF
+WORD_SIZE = 16
 
 def reverse_bits(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    # assume 64-bit arg
+    result = 0
+    for _ in range(4):
+        result = (result << WORD_SIZE) + LUT_16[x & MASK]
+        x >>= WORD_SIZE
+
+    return result
 
 
 if __name__ == '__main__':
