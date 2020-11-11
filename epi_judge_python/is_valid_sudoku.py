91869c195bec83694dc608a9e492f9eccd128b78
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-11-11 14:16:43 -0500

    Unfinished 5.17 Sudoku checker (p. 66)

--- epi_judge_python/is_valid_sudoku.py
+++ epi_judge_python/is_valid_sudoku.py
@@ -5,7 +5,34 @@ from test_framework import generic_test
 
 # Check if a partially filled matrix has any conflicts.
 def is_valid_sudoku(partial_assignment: List[List[int]]) -> bool:
-    # TODO - you fill in here.
+    def validate_rows(m):
+        for row in m:
+            ht = [0] * 10
+            for e in row:
+                if e != 0 and ht[e]:
+                    return False
+                else:
+                    ht[e] = 1
+        return True
+
+    # TODO
+    def validate_square(m):
+        return True
+
+    # check rows
+    if not validate_rows(partial_assignment): return False
+
+    # transpose matrix to apply validate_rows to columns
+    transposed = list(zip(*partial_assignment))
+    if not validate_rows(transposed): return False
+
+
+    ## TODO
+    # for x in range(3):
+    #     for y in range(3):
+    #         m = partial_assignment[x*3:x*3+3, y*3:y*3+3]
+    #         if not validate_square(m): return False
+
     return True
 
 
