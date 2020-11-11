335f4e5179e559a3a345a2161bdd622d8f3b38a2
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-11-11 13:39:37 -0500

    Solution to 5.2 increment an integer stored as array (p. 47)

--- epi_judge_python/int_as_array_increment.py
+++ epi_judge_python/int_as_array_increment.py
@@ -4,8 +4,19 @@ from test_framework import generic_test
 
 
 def plus_one(A: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    A.reverse()
+
+    i=0
+    while i < len(A) and A[i] == 9:
+        A[i] = 0
+        i += 1
+    
+    if i == len(A): # every number was a 9
+        A.append(1)
+    else:
+        A[i] += 1
+
+    return A[::-1]
 
 
 if __name__ == '__main__':
