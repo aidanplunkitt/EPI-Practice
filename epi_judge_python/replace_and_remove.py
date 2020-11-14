9965eafa60fdd0f7dce6ddc8d6c370254621f6af
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-11-14 12:45:12 -0500

    Initial solution to 6.4 replace and remove

--- epi_judge_python/replace_and_remove.py
+++ epi_judge_python/replace_and_remove.py
@@ -6,8 +6,30 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def replace_and_remove(size: int, s: List[str]) -> int:
-    # TODO - you fill in here.
-    return 0
+    num_a = 0
+    last_nonb = 0
+    for i in range(size):
+        if s[i] != 'b':
+            if s[i] == 'a':
+                num_a += 1
+            s[i], s[last_nonb] = s[last_nonb], s[i]
+            last_nonb += 1
+
+    # [a,c,d,c,a,a,d,_,_,_,_]
+    #                ^ last_nonb
+
+    i = last_nonb + num_a - 1
+    j = last_nonb - 1
+    while j >= 0:
+        s[i] = s[j]
+        i -= 1
+        if s[j] == 'a':
+            s[i+1] = 'd'
+            s[i] = 'd'
+            i -= 1
+        j -= 1
+
+    return last_nonb + num_a
 
 
 @enable_executor_hook
