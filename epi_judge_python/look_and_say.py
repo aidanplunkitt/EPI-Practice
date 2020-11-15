e57f1869db655abbe19d7a432d5b56347d7809cb
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-11-15 15:32:40 -0500

    Add pythonic iterator solution to 6.8 (p. 83)

--- epi_judge_python/look_and_say.py
+++ epi_judge_python/look_and_say.py
@@ -1,22 +1,10 @@
 from test_framework import generic_test
-
+import itertools
 
 def look_and_say(n: int) -> str:
-    def next_number(s):
-        result, i = [], 0
-        while i < len(s):
-            count = 1
-            while i + 1 < len(s) and s[i] == s[i+1]:
-                i += 1
-                count += 1
-            result.append(str(count) + s[i])
-            i += 1
-
-        return ''.join(result)
-
     s = '1'
-    for _ in range(1, n):
-        s = next_number(s)
+    for _ in range(n - 1):
+        s = ''.join(str(len(list(group))) + key for key, group in itertools.groupby(s))
     return s
 
 if __name__ == '__main__':
