cd81606f04cb731d0bbab144840c1a3617b82cbf
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-11-15 13:10:08 -0500

    Initial solution to 6.7 phone number mnemonics

--- epi_judge_python/phone_number_mnemonic.py
+++ epi_judge_python/phone_number_mnemonic.py
@@ -2,10 +2,17 @@ from typing import List
 
 from test_framework import generic_test, test_utils
 
+import string
+
+phone_map = [['0'], ['1'], ['A','B','C'], ['D','E','F'], ['G','H','I'], ['J','K','L'], ['M','N','O'], ['P','Q','R','S'], ['T','U','V'], ['W','X','Y','Z']]
 
 def phone_mnemonic(phone_number: str) -> List[str]:
-    # TODO - you fill in here.
-    return []
+    num = string.digits.index(phone_number[-1])
+
+    if len(phone_number) == 1:
+        return phone_map[num]
+
+    return [A + b for A in phone_mnemonic(phone_number[:-1]) for b in phone_map[num]]    
 
 
 if __name__ == '__main__':
