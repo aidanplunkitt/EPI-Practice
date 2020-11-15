def54b9c482f1a9e457e913af489aae4d6540a73
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-11-15 12:19:14 -0500

    Initial solution to 6.5 reverse words in a string

--- epi_judge_python/reverse_words.py
+++ epi_judge_python/reverse_words.py
@@ -7,7 +7,22 @@ from test_framework.test_utils import enable_executor_hook
 # Assume s is a list of strings, each of which is of length 1, e.g.,
 # ['r', 'a', 'm', ' ', 'i', 's', ' ', 'c', 'o', 's', 't', 'l', 'y'].
 def reverse_words(s):
-    # TODO - you fill in here.
+    for i in range(len(s) // 2):
+        s[i], s[~i] = s[~i], s[i]
+
+    i = j = 0
+    while i < len(s):
+        while j < len(s) and s[j] != ' ': 
+            j += 1
+
+        # back up one
+        k = j - 1
+        while i < k:
+            s[i], s[k] = s[k], s[i]
+            i, k = i + 1, k - 1
+        
+        i = j = j + 1
+
     return
 
 
