883df56219450b373c32327ba8786891884d1bf2
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-11-15 14:28:53 -0500

    Initial solution to 6.8 look-and-say

--- epi_judge_python/look_and_say.py
+++ epi_judge_python/look_and_say.py
@@ -2,8 +2,24 @@ from test_framework import generic_test
 
 
 def look_and_say(n: int) -> str:
-    # TODO - you fill in here.
-    return ''
+    if n == 0: return ''
+    if n == 1: return '1'
+    if n == 2: return '11'
+    
+    a, b = [], [1, 1]
+    j = 1
+    for _ in range(n-2):
+        a, b = b, []
+        j = 1
+        for i in range(1,len(a)):
+            if a[i] == a[i-1]:
+                j += 1
+            else:
+                b.extend([j, a[i-1]])
+                j = 1
+        b.extend([j, a[-1]])
+
+    return ''.join(str(x) for x in b)
 
 
 if __name__ == '__main__':
