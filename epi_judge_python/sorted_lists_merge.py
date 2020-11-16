b0275a10ed67f0cbb3ae3224256f85cd3bd28d41
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-11-16 09:25:28 -0500

    Initial solution to 7.1 merge sorted LLs

--- epi_judge_python/sorted_lists_merge.py
+++ epi_judge_python/sorted_lists_merge.py
@@ -6,8 +6,26 @@ from test_framework import generic_test
 
 def merge_two_sorted_lists(L1: Optional[ListNode],
                            L2: Optional[ListNode]) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    l = ListNode()
+    ph = l
+
+    while L1 or L2:
+        if not L1:
+            l.next = L2
+            L2 = L2.next
+        elif not L2:
+            l.next = L1
+            L1 = L1.next
+        else:
+            if L1.data < L2.data:
+                l.next = L1
+                L1 = L1.next
+            else:
+                l.next = L2
+                L2 = L2.next
+        l = l.next
+
+    return ph.next
 
 
 if __name__ == '__main__':
