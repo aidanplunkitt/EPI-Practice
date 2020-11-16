aa4125d2a999375d51b10314e2d18773fd741c18
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-11-16 10:10:01 -0500

    Initial solution to 7.2 reverse a sublist within a LL

--- epi_judge_python/reverse_sublist.py
+++ epi_judge_python/reverse_sublist.py
@@ -6,8 +6,24 @@ from test_framework import generic_test
 
 def reverse_sublist(L: ListNode, start: int,
                     finish: int) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    if (finish - start) < 1: return L
+
+    ph = ListNode()
+    ph.next = L
+    L = ph
+
+    for _ in range(1, start):
+        L = L.next
+
+    prev, r_tail, t = L, L.next, L.next.next
+    r_head, t_next = r_tail, t
+    for _ in range(finish - start):
+        t_next = t.next
+        t.next = r_head
+        r_head = t
+        t = t_next
+    prev.next, r_tail.next = r_head, t
+    return ph.next
 
 
 if __name__ == '__main__':
