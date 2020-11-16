04cd81247f47efdb656d94a74dec459818eb6598
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-11-16 12:14:18 -0500

    Initial solution to 7.10 even-odd index LL merge

--- epi_judge_python/even_odd_list_merge.py
+++ epi_judge_python/even_odd_list_merge.py
@@ -5,8 +5,22 @@ from test_framework import generic_test
 
 
 def even_odd_merge(L: ListNode) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    if not (L and L.next): return L
+
+    even, odd, L = L, L.next, L.next.next
+    h_even, h_odd = even, odd
+    
+    while L:
+        even.next = L
+        even = even.next
+        L = L.next
+        if L:
+            odd.next = L
+            odd = odd.next
+            L = L.next
+
+    even.next, odd.next = h_odd, None
+    return h_even
 
 
 if __name__ == '__main__':
