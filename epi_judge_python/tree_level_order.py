6d7ca32f438b9347f55ff002bdf3fa9b0992fae1
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-11-17 18:19:48 -0500

    Solution to 8.6 order nodes by depth

--- epi_judge_python/tree_level_order.py
+++ epi_judge_python/tree_level_order.py
@@ -2,11 +2,17 @@ from typing import List
 
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
+import collections
 
 
 def binary_tree_depth_order(tree: BinaryTreeNode) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    ans, q = [], [tree]
+
+    while q:
+        ans.append([n.data for n in q])
+        q = [child for node in q for child in (node.left, node.right) if child]
+
+    return ans
 
 
 if __name__ == '__main__':
