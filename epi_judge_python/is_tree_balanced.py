865ff4b76d4da9c5d3e2e1c21d83b929ea1eca35
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-11-18 09:55:37 -0500

    Solution to 9.1 check if binary tree is balanced

--- epi_judge_python/is_tree_balanced.py
+++ epi_judge_python/is_tree_balanced.py
@@ -3,8 +3,21 @@ from test_framework import generic_test
 
 
 def is_balanced_binary_tree(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
-    return True
+    def get_height(subtree):
+        left, isBalanced = get_height(subtree.left) if subtree.left else (0, True)
+        if not isBalanced: return 0, False
+
+        right, isBalanced = get_height(subtree.right) if subtree.right else (0, True)
+        if not isBalanced: return 0, False
+
+        if abs(left - right) > 1:
+            return 0, False
+
+        return max(left + 1, right + 1), True
+
+    if not tree: return True
+    _, isBalanced = get_height(tree)
+    return isBalanced
 
 
 if __name__ == '__main__':
