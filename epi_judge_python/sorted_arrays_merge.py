4529960c6fd911fb81f545ca06fa4a209a1a5e06
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-11-20 18:41:42 -0500

    Broken initial solution to 10.1
    Wonky algorithm to try to stick to the problem specification in the book
    which is 500 txt files containing only positive integers representing
    timestamps, which cannot all fit into RAM simultaneously

--- epi_judge_python/sorted_arrays_merge.py
+++ epi_judge_python/sorted_arrays_merge.py
@@ -2,10 +2,27 @@ from typing import List
 
 from test_framework import generic_test
 
+import heapq
 
 def merge_sorted_arrays(sorted_arrays: List[List[int]]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    MAX_NUM_HEAP_ELEMENTS = 100
+    combined, final = [0] * MAX_NUM_HEAP_ELEMENTS, []
+
+    i, items_per_file = 0, MAX_NUM_HEAP_ELEMENTS // len(sorted_arrays)
+    for a in sorted_arrays:
+        # find a way to use prevmax to skip past already included items in individual subarrays, and find a smarter way
+        # to fill up heap from the subarrays than with a precalculated constant num of elems from each subarray
+        if (items_per_file * i) < len(a):
+            for item in a[(items_per_file * i):min(items_per_file * (i+1), len(a))]:
+                # make negative to use minheap as maxheap
+                if (item * -1) < combined[0]:
+                    heapq.heappushpop(combined, item * -1)
+
+        i += 1
+        combined = [n * -1 for n in combined if n < 0]
+        final.extend(combined)
+        # prevmax = combined[0]
+    return final
 
 
 if __name__ == '__main__':
