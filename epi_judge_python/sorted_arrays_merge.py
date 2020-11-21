0821a7e893d8aab3883767f2f09d355ab0ca90be
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-11-20 21:53:08 -0500

    Add working solution using minheap instead of maxheap
    O(n log k) time, where k is the number of arrays in sorted_arrays
    O(k) space to hold an iterator for each array in memory

--- epi_judge_python/sorted_arrays_merge.py
+++ epi_judge_python/sorted_arrays_merge.py
@@ -5,24 +5,21 @@ from test_framework import generic_test
 import heapq
 
 def merge_sorted_arrays(sorted_arrays: List[List[int]]) -> List[int]:
-    MAX_NUM_HEAP_ELEMENTS = 100
-    combined, final = [0] * MAX_NUM_HEAP_ELEMENTS, []
+    minheap, iters = [], []
+    for i, arr in enumerate(sorted_arrays):
+        iters.append(iter(arr))
+        heapq.heappush(minheap, (next(iters[i], None), i))
 
-    i, items_per_file = 0, MAX_NUM_HEAP_ELEMENTS // len(sorted_arrays)
-    for a in sorted_arrays:
-        # find a way to use prevmax to skip past already included items in individual subarrays, and find a smarter way
-        # to fill up heap from the subarrays than with a precalculated constant num of elems from each subarray
-        if (items_per_file * i) < len(a):
-            for item in a[(items_per_file * i):min(items_per_file * (i+1), len(a))]:
-                # make negative to use minheap as maxheap
-                if (item * -1) < combined[0]:
-                    heapq.heappushpop(combined, item * -1)
+    sort = []
+    while minheap:
+        smallest, smallest_i = heapq.heappop(minheap)
+        sort.append(smallest)
+        topush = next(iters[smallest_i], None)
+        if topush is not None:
+            heapq.heappush(minheap, (topush, smallest_i))
 
-        i += 1
-        combined = [n * -1 for n in combined if n < 0]
-        final.extend(combined)
-        # prevmax = combined[0]
-    return final
+    return sort
+
 
 
 if __name__ == '__main__':
