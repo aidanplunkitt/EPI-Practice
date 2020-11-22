392f9b9f3d703f3e0e5cb861796be60a83559824
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-11-22 14:26:38 -0500

    Add hand-implemented bisection for 11.1

--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -6,10 +6,24 @@ import bisect
 
 # 11.1, O(log n) time, O(1) space
 def search_first_of_k(A: List[int], k: int) -> int:
-    index = bisect.bisect_left(A, k)
-    if index == len(A) or (A[index] != k):
-        index = -1
-    return index
+    left, right, result = 0, len(A) - 1, -1
+    while left <= right:
+        mid = (left + right) // 2
+        if A[mid] == k:
+            result = mid
+            right = mid - 1
+        elif A[mid] > k:
+            right = mid - 1
+        else: # A[mid] < k
+            left = mid + 1
+
+    return result
+
+# Pythonic solution:
+def pythonic_search_first_of_k(A: List[int], k: int) -> int:
+    i = bisect.bisect_left(A, k)
+    return i if i < len(A) and A[i] == k else -1
+
 
 
 if __name__ == '__main__':
