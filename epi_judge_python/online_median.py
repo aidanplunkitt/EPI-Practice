0394ffaded75b21221660d0eb3182286d7a8c277
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-11-21 20:24:31 -0500

    Solution to 10.5 running median

--- epi_judge_python/online_median.py
+++ epi_judge_python/online_median.py
@@ -2,10 +2,22 @@ from typing import Iterator, List
 
 from test_framework import generic_test
 
+import heapq
 
+# 10.5, O(n log n) time, O(n) space
 def online_median(sequence: Iterator[int]) -> List[float]:
-    # TODO - you fill in here.
-    return []
+    # lower_half is a maxheap
+    ans, lower_half, higher_half = [], [], []
+
+    for s in sequence:
+        heapq.heappush(lower_half, heapq.heappushpop(higher_half, s) * -1)
+
+        if len(lower_half) > len(higher_half):
+            heapq.heappush(higher_half, heapq.heappop(lower_half) * -1)
+
+        ans.append(higher_half[0] if len(higher_half) > len(lower_half) else (0.5 * (lower_half[0] * -1 + higher_half[0])))
+
+    return ans
 
 
 def online_median_wrapper(sequence):
