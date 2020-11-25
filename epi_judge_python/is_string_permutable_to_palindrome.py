12bd67a0cb6ba3feeb24f8f8c609ac35ab76631e
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-11-25 17:35:17 -0500

    Pythonic solution to 12.1

--- epi_judge_python/is_string_permutable_to_palindrome.py
+++ epi_judge_python/is_string_permutable_to_palindrome.py
@@ -4,14 +4,7 @@ import collections
 
 # 12.1, O(n) time, O(n) space
 def can_form_palindrome(s: str) -> bool:
-    letters = collections.Counter(c for c in s if c.isalpha())
-
-    odd = 0
-    for _, count in letters.items():
-        if count % 2 == 1:
-            odd += 1
-
-    return odd < 2
+    return sum(n % 2 for n in collections.Counter(s).values()) < 2
 
 
 if __name__ == '__main__':
