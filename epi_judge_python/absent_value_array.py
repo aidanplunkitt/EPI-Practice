e31daf265ec00822cc7d909377f6bcd4a1b95ab6
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-11-25 13:01:04 -0500

    Solution to 11.9, generate an IP address that does not appear in a file of 1 billion IP addresses

--- epi_judge_python/absent_value_array.py
+++ epi_judge_python/absent_value_array.py
@@ -3,9 +3,36 @@ from typing import Iterator
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 
+import itertools
 
+# 11.9, O(n) time, O(2^16 * size(int) space)
 def find_missing_element(stream: Iterator[int]) -> int:
-    # TODO - you fill in here.
+    # copy iterator into queues so multiple passes can be made
+    stream, stream_copy = itertools.tee(stream)
+
+    # identify a 16-bit prefix with fewer than 2^16 (max) number of appearances in stream
+    prefix_counts = [0] * 2**16
+    for num in stream:
+        prefix_counts[num >> 16] += 1
+
+    prefix = 0
+    for num in prefix_counts:
+        if num < (2 ** 16):
+            prefix = num
+            break
+
+
+    # identify a number with specified prefix that does not appear in stream
+    suffix_counts = [0] * 2**16
+    for num in stream_copy:
+        if (num >> 16) == prefix:
+            suffix_counts[num & 0xFFFF] = 1
+
+    for i, num in enumerate(suffix_counts):
+        if num == 0:
+            return prefix << 16 + i
+
+    # shouldn't get here
     return 0
 
 
