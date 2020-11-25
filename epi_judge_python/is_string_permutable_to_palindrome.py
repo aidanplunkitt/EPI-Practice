12ffb014352ccfc6caeb3034f81993b2784a98c6
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-11-25 17:33:02 -0500

    Initial solution to 12.1 see if word is anagram of a palindrome

--- epi_judge_python/is_string_permutable_to_palindrome.py
+++ epi_judge_python/is_string_permutable_to_palindrome.py
@@ -1,9 +1,17 @@
 from test_framework import generic_test
 
+import collections
 
+# 12.1, O(n) time, O(n) space
 def can_form_palindrome(s: str) -> bool:
-    # TODO - you fill in here.
-    return True
+    letters = collections.Counter(c for c in s if c.isalpha())
+
+    odd = 0
+    for _, count in letters.items():
+        if count % 2 == 1:
+            odd += 1
+
+    return odd < 2
 
 
 if __name__ == '__main__':
