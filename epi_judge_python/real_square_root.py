4c369fb1935b73bae781b5facd26c9a0f2442e24
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-11-25 13:22:17 -0500

    Solution to 11.5 find square root of float

--- epi_judge_python/real_square_root.py
+++ epi_judge_python/real_square_root.py
@@ -1,9 +1,26 @@
 from test_framework import generic_test
 
+import math
 
+# 11.5, O(log x / s) time, where s it the tolerance in math.isclose(), O(1) space
 def square_root(x: float) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    left, right = 1.0, x
+
+    if x < 1.0:
+        right, left = left, right
+
+    while left <= right:
+        mid = 0.5 * (left + right)
+        square = mid * mid
+        if math.isclose(x, square):
+            return mid
+        elif square > x:
+            right = mid
+        else:
+            left = mid
+
+    # shouldn't get here
+    return -1.0
 
 
 if __name__ == '__main__':
