eea9c1e8c03cef20e4a5c72e852cedc6252ea165
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-11-25 20:25:22 -0500

    Initial solution to 12.4 LCA with tighter upper bound on time

--- epi_judge_python/lowest_common_ancestor_close_ancestor.py
+++ epi_judge_python/lowest_common_ancestor_close_ancestor.py
@@ -7,11 +7,24 @@ from test_framework.binary_tree_utils import must_find_node
 from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
-
+# 12.4, O(max(depth0, depth1)) time, where depthn is the depth to the lca, O(d0 + d1) space where dn is the num of nodes to the lca
 def lca(node0: BinaryTreeNode,
         node1: BinaryTreeNode) -> Optional[BinaryTreeNode]:
-    # TODO - you fill in here.
-    return None
+    nodeset = set()
+    while node0.parent or node1.parent:
+        if node0 in nodeset:
+            return node0
+        if  node0.parent:
+            nodeset.add(node0)
+            node0 = node0.parent
+
+        if node1 in nodeset:
+            return node1
+        if node1.parent:
+            nodeset.add(node1)
+            node1 = node1.parent
+
+    return node0 # root
 
 
 @enable_executor_hook
