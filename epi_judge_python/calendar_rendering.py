c33a64a0ef886ea36ebf9cba674cc9d147dacead
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-11-27 10:59:37 -0500

    Solution to 13.6 find max number of simulatenous events

--- epi_judge_python/calendar_rendering.py
+++ epi_judge_python/calendar_rendering.py
@@ -8,11 +8,24 @@ from test_framework.test_utils import enable_executor_hook
 # Event is a tuple (start_time, end_time)
 Event = collections.namedtuple('Event', ('start', 'finish'))
 
-
+# 13.6, O(n log n) time, O(n) space
 def find_max_simultaneous_events(A: List[Event]) -> int:
-    # TODO - you fill in here.
-    return 0
+    times = []
+    for event in A:
+        times.append((event.start, True))
+        times.append((event.finish, False))
 
+    times.sort(key=lambda time: (time[0], not time[1]))
+
+    running_sum = max_sum = 0
+    for time in times:
+        if time[1]:
+            running_sum += 1
+            max_sum = max(max_sum, running_sum)
+        else:
+            running_sum -= 1
+
+    return max_sum
 
 @enable_executor_hook
 def find_max_simultaneous_events_wrapper(executor, events):
