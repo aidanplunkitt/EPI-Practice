9fc09b767ed2b0ebdd1ef83009b5d943f35693b4
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-11-27 13:06:14 -0500

    Initial solution to 13.11 merge sort for linked list

--- epi_judge_python/sort_list.py
+++ epi_judge_python/sort_list.py
@@ -3,10 +3,28 @@ from typing import Optional
 from list_node import ListNode
 from test_framework import generic_test
 
-
+# 13.11, O(n log n) time, O(log n) space (implicit callstack space)
 def stable_sort_list(L: ListNode) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    if L is None or L.next is None:
+        return L
+
+    mid, tail = L, L.next
+    while tail is not None and tail.next is not None:
+        mid = mid.next
+        tail = tail.next.next
+
+    R, mid.next = mid.next, None
+    L, R = stable_sort_list(L), stable_sort_list(R)
+    dh = node = ListNode()
+    while L and R:
+        if L.data < R.data:
+            node.next, L = L, L.next
+        else:
+            node.next, R = R, R.next
+        node = node.next
+
+    node.next = L or R
+    return dh.next
 
 
 if __name__ == '__main__':
