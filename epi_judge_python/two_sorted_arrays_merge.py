2e8e52abf1b17d9e15b6c7e057735c6c7a815d6c
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-11-27 09:36:15 -0500

    Initial solution to 13.2 merge sorted arrays in-place

--- epi_judge_python/two_sorted_arrays_merge.py
+++ epi_judge_python/two_sorted_arrays_merge.py
@@ -2,11 +2,24 @@ from typing import List
 
 from test_framework import generic_test
 
-
+# 13.2, O(A + B) time and space
 def merge_two_sorted_arrays(A: List[int], m: int, B: List[int],
                             n: int) -> None:
-    # TODO - you fill in here.
-    return
+    a, b = m - 1, n - 1
+    i = len(A) - 1
+    while i >= 0:
+        if a < 0:
+            A[i] = B[b]
+            b -= 1
+        elif b < 0:
+            i = -1 # return early, rest of A already in place
+        elif A[a] >= B[b]:
+            A[i] = A[a]
+            a -= 1
+        else:
+            A[i] = B[b]
+            b -= 1
+        i -= 1
 
 
 def merge_two_sorted_arrays_wrapper(A, m, B, n):
