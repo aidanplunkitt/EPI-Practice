95c5810bf7e5f750e391c53fb93d191bf2a71279
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-11-28 14:33:30 -0500

    Solution to 15.2 n queens

--- epi_judge_python/n_queens.py
+++ epi_judge_python/n_queens.py
@@ -2,10 +2,27 @@ from typing import List
 
 from test_framework import generic_test
 
-
+# 15.2
 def n_queens(n: int) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    def solve_n_queens(row):
+        if row == n:
+            valid_placements.append(board.copy())
+            return
+        for col in range(n):
+            if all(
+                    # if not in a vertical or diagonal line with a queen from an above row
+                    abs(c - col) not in (0, row - i) 
+                    # for every previous row
+                    for i, c in enumerate(board[:row])):
+                # mark this as a valid placement for this row
+                board[row] = col
+                # solve for next row
+                solve_n_queens(row + 1)
+
+    valid_placements = []
+    board = [0] * n
+    solve_n_queens(0)
+    return valid_placements
 
 
 def comp(a, b):
