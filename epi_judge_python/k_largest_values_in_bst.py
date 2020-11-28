7d4ff9dd5df34935a9e3aa91c58f565fa3d6ea5d
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-11-27 22:00:41 -0500

    Initial solution to 14.3 find k largest keys in BST

--- epi_judge_python/k_largest_values_in_bst.py
+++ epi_judge_python/k_largest_values_in_bst.py
@@ -3,10 +3,33 @@ from typing import List
 from bst_node import BstNode
 from test_framework import generic_test, test_utils
 
-
+# 14.3, O(n) time, O(1) space
 def find_k_largest_in_bst(tree: BstNode, k: int) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    def get_next(node):
+        if node.left:
+            node = node.left
+            while node.right:
+                node = node.right
+            return node
+
+        while node.parent and node.parent.left is node:
+            node = node.parent
+        return node.parent
+
+    if not tree: return []
+
+    # start at max
+    while tree.right:
+        tree = tree.right
+
+    # reverse in-order walk
+    largest = []
+    for _ in range(k):
+        largest.append(tree.data)
+        tree = get_next(tree)
+        if not tree: break
+
+    return largest
 
 
 if __name__ == '__main__':
