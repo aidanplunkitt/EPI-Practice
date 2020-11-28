de14a7f79b2f2f7c7e34b13be9adc98361eb88b2
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-11-28 08:45:01 -0500

    Initial solution to 14.5 reconstruct BST from preorder list

--- epi_judge_python/bst_from_preorder.py
+++ epi_judge_python/bst_from_preorder.py
@@ -3,11 +3,29 @@ from typing import List, Optional
 from bst_node import BstNode
 from test_framework import generic_test
 
-
+# 14.5, O(n^2) time, O(n) space
 def rebuild_bst_from_preorder(preorder_sequence: List[int]
                               ) -> Optional[BstNode]:
-    # TODO - you fill in here.
-    return None
+    if not preorder_sequence: return None
+    root = BstNode(preorder_sequence[0])
+    for i in range(1, len(preorder_sequence)):
+        node = prev_node = root
+        val = preorder_sequence[i]
+        left = val < node.data
+        while node:
+            prev_node = node
+            if val < node.data:
+                node, left = node.left, True
+            else:
+                node, left = node.right, False
+        new_node = BstNode(val)
+        if left:
+            prev_node.left  = new_node
+        else:
+            prev_node.right = new_node
+
+    return root
+
 
 
 if __name__ == '__main__':
