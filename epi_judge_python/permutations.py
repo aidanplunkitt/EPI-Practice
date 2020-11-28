91a28ec841eab09348fac4fdc5f863581a82541e
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-11-28 14:47:00 -0500

    Initial solution to 15.3 generate all permutations of a list

--- epi_judge_python/permutations.py
+++ epi_judge_python/permutations.py
@@ -2,10 +2,19 @@ from typing import List
 
 from test_framework import generic_test, test_utils
 
-
+# 15.3, O(n*n!) time, O(n^2) space?
 def permutations(A: List[int]) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    def helper(elems, perms):
+        if not elems:
+            permutations.append(perms)
+            return
+
+        for i, a in enumerate(elems):
+            helper(elems[:i] + elems[i+1:], perms + [a])
+
+    permutations = []
+    helper(A, [])
+    return permutations
 
 
 if __name__ == '__main__':
