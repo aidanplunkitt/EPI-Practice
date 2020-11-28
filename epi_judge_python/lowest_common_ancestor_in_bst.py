3eb8e43b0cec6f0b27a51810886c87716a3555b9
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-11-28 08:10:57 -0500

    Initial solution to 14.4 find LCA of nodes in a BST

--- epi_judge_python/lowest_common_ancestor_in_bst.py
+++ epi_judge_python/lowest_common_ancestor_in_bst.py
@@ -9,10 +9,12 @@ from test_framework.test_utils import enable_executor_hook
 
 
 # Input nodes are nonempty and the key at s is less than or equal to that at b.
+# 14.4, O(min(height_s,height_b)) time, O(1) space
 def find_lca(tree: BstNode, s: BstNode, b: BstNode) -> Optional[BstNode]:
-    # TODO - you fill in here.
-    return None
+    while (tree.data > s.data and tree.data > b.data) or (tree.data < s.data and tree.data < b.data):
+        tree = tree.left if tree.data > s.data else tree.right
 
+    return tree
 
 @enable_executor_hook
 def lca_wrapper(executor, tree, s, b):
