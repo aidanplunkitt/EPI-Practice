225b53f1e6ede8814f1be47a080933c83aff51dc
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-12-05 17:21:48 -0500

    Fibonacci passing reference to list

--- epi_judge_python/fibonacci.py
+++ epi_judge_python/fibonacci.py
@@ -2,14 +2,10 @@ from test_framework import generic_test
 
 # 16.0, O(n) time, O(n) space for cache
 def fibonacci(n: int) -> int:
-    cache = [0,1] + [-1] * (n-1)
-    def helper(i):
-        if i < 2:
-            return i
-        if cache[i] != -1:
+    def helper(i, cache=[0,1]):
+        if i < len(cache):
             return cache[i]
-
-        cache[i] = helper(i-1) + helper(i-2)
+        cache.append(helper(i-1) + helper(i-2))
         return cache[i]
 
     return helper(n)
