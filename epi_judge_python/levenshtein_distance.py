bcfc3c8aef6c7b0321a5819fb02bdbd6aabc4a44
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-12-05 16:46:06 -0500

    O(min(a,b)) space solution for 16.2

--- epi_judge_python/levenshtein_distance.py
+++ epi_judge_python/levenshtein_distance.py
@@ -1,25 +1,22 @@
 from test_framework import generic_test
 
-# 16.2, O(ab) time and space
+# 16.2, O(ab) time, O(min(a,b)) space
 def levenshtein_distance(A: str, B: str) -> int:
-    def compute_distance(a, b):
-        if a < 0:
-            return b + 1
-        elif b < 0:
-            return a + 1
-
-        if matrix[a][b] == -1:
-            if A[a] == B[b]:
-                matrix[a][b] = compute_distance(a-1, b-1)
-            else:
-                replace = compute_distance(a-1, b-1) # swap val in B for val in A (B[b] = A[a])
-                add     = compute_distance(a-1, b) # add A[a] to B (insert at B[b+1])
-                delete  = compute_distance(a  , b-1) # delete from B (remove B[b])
-                matrix[a][b] = min(replace, add, delete) + 1
-        return matrix[a][b]
-
-    matrix = [[-1] * len(B) for _ in A]
-    return compute_distance(len(A) - 1, len(B) - 1)
+    # for min(A,B) space
+    if len(A) < len(B):
+        A, B = B, A
+    distances = list(range(len(B)+1))
+    for A_idx in range(1, len(A)+1):
+        diag = distances[0]
+        distances[0] = A_idx
+        for B_idx in range(1, len(B)+1):
+            next_diag = distances[B_idx]
+            if B[B_idx - 1] == A[A_idx - 1]:
+                distances[B_idx] = diag
+            else: 
+                distances[B_idx] = 1 + min(diag, distances[B_idx], distances[B_idx - 1]) # diag, up, left
+            diag = next_diag
+    return distances[-1]
 
 
 if __name__ == '__main__':
