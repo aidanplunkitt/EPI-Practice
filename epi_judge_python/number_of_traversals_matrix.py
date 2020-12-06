a135a93cd956d3075ad48cd0b90a3b8bf5b81189
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-12-05 20:29:43 -0500

    Initial solution to 16.3 number of ways to traverse a matrix

--- epi_judge_python/number_of_traversals_matrix.py
+++ epi_judge_python/number_of_traversals_matrix.py
@@ -1,9 +1,19 @@
 from test_framework import generic_test
 
-
+# 16.3, O(nm) time and space
 def number_of_ways(n: int, m: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    matrix = [[0] * m for _ in range(n)]
+
+    for i in range(m):
+        matrix[0][i] = 1
+    for i in range(n):
+        matrix[i][0] = 1
+
+    for i in range(1, n):
+        for j in range(1, m):
+            matrix[i][j] = matrix[i][j-1] + matrix[i-1][j]
+
+    return matrix[-1][-1]
 
 
 if __name__ == '__main__':
