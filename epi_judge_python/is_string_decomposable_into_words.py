c4acaddd6f1a0446ab9e5c9f1c14d6b44e3b325c
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-12-08 14:59:24 -0500

    Solution to 16.7 bedbathandbeyond.com problem

--- epi_judge_python/is_string_decomposable_into_words.py
+++ epi_judge_python/is_string_decomposable_into_words.py
@@ -5,11 +5,27 @@ from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
-
+# 16.7, O(n^3) time (n being length of domain), O(n) space
 def decompose_into_dictionary_words(domain: str,
                                     dictionary: Set[str]) -> List[str]:
-    # TODO - you fill in here.
-    return []
+    word_lens = [-1] * len(domain)
+    for i in range(len(domain)):
+        if domain[:i+1] in dictionary:
+            word_lens[i] = i + 1
+            continue
+
+        for j in range(i):
+            if word_lens[j] != -1 and domain[j+1:i+1] in dictionary:
+                word_lens[i] = i - j
+                break
+
+    words = []
+    if word_lens[-1] != -1:
+        i = len(domain) - 1
+        while i >= 0:
+            words.append(domain[i + 1 - word_lens[i]:i + 1])
+            i -= word_lens[i]
+    return words[::-1]
 
 
 @enable_executor_hook
