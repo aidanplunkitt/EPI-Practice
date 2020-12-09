debc5132584a4115b0778cc9e3274b61f0f14a8a
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-12-09 15:01:43 -0500

    O(1) space solution for three-sum (and two-sum)

--- epi_judge_python/three_sum.py
+++ epi_judge_python/three_sum.py
@@ -2,18 +2,22 @@ from typing import List
 
 from test_framework import generic_test
 
-# 17.4, O(n^2) time, O(n) space
+# 17.4, O(n^2) time, O(1) space
 def has_three_sum(A: List[int], t: int) -> bool:
-    nums = set()
-    for a in A:
-        nums.add(t - a)
-
-    for i in range(len(A)):
-        for j in range(i, len(A)):
-            if (A[i] + A[j]) in nums:
+    def has_two_sum(A, t):
+        i, j = 0, len(A) - 1
+        while i <= j:
+            n = A[i] + A[j]
+            if n < t:
+                i += 1
+            elif n > t:
+                j -= 1
+            else:
                 return True
-            
-    return False
+        return False
+
+    A.sort()
+    return any(has_two_sum(A, t-a) for a in A)
 
 
 if __name__ == '__main__':
