0962236209e3ca057abd3669e71ce106cef669a2
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-12-11 16:25:53 -0500

    Solution to 17.7 max area formed by array of heights

--- epi_judge_python/max_trapped_water.py
+++ epi_judge_python/max_trapped_water.py
@@ -2,10 +2,17 @@ from typing import List
 
 from test_framework import generic_test
 
-
+# 17.7, O(n) time, O(1) space
 def get_max_trapped_water(heights: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    i, j = 0, len(heights) - 1
+    max_area = 0
+    while i < j:
+        max_area = max(max_area, (j - i) * min(heights[i], heights[j]))
+        if heights[i] <= heights[j]:
+            i += 1
+        else:
+            j -= 1
+    return max_area
 
 
 if __name__ == '__main__':
