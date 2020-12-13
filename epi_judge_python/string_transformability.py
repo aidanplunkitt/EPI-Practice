3d55290bc720e71cde9f8c2064ff51921afe684d
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-12-12 21:40:15 -0500

    Initial (not working) solution to 18.7 string transform
    Finds a path if one exists but does not find shortest path

--- epi_judge_python/string_transformability.py
+++ epi_judge_python/string_transformability.py
@@ -2,11 +2,35 @@ from typing import Set
 
 from test_framework import generic_test
 
+import collections
+import string
 
+# 18.7, O(s * D) time, O(D^2) space ?
 def transform_string(D: Set[str], s: str, t: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    q = collections.deque([s])
+    graph = {}
+    seen = set()
+    while q:
+        orig = q.popleft()
+        seen.add(orig)
+        if orig == t:
+            key, count = t, 0
+            while key != s:
+                key = graph[key]
+                count += 1
+            return count
 
+        word = list(orig)
+        for i, c in enumerate(word):
+            for l in string.ascii_lowercase:
+                word[i] = l
+                key = ''.join(word)
+                if key in D and key not in seen:
+                    q.append(key)
+                    graph[key] = orig
+            word[i] = c
+    
+    return -1
 
 if __name__ == '__main__':
     exit(
