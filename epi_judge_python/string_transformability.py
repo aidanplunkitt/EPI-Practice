60999a1bd2b921d98c61a02423d4be3b06ce8a6d
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-12-15 14:20:45 -0500

    Initial working solution to 18.7

--- epi_judge_python/string_transformability.py
+++ epi_judge_python/string_transformability.py
@@ -7,27 +7,20 @@ import string
 
 # 18.7, O(s * D) time, O(D^2) space ?
 def transform_string(D: Set[str], s: str, t: str) -> int:
-    q = collections.deque([s])
-    graph = {}
+    q = collections.deque([(s, 0)])
     seen = set()
     while q:
-        orig = q.popleft()
+        orig, num = q.popleft()
         seen.add(orig)
-        if orig == t:
-            key, count = t, 0
-            while key != s:
-                key = graph[key]
-                count += 1
-            return count
-
         word = list(orig)
         for i, c in enumerate(word):
             for l in string.ascii_lowercase:
                 word[i] = l
                 key = ''.join(word)
                 if key in D and key not in seen:
-                    q.append(key)
-                    graph[key] = orig
+                    if key == t:
+                        return num + 1
+                    q.append((key, num + 1))
             word[i] = c
     
     return -1
