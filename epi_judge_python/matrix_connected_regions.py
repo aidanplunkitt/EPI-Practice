6124f2aa54afc19fa6c976709adafa59e0bc2742
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-12-15 19:49:56 -0500

    DFS solution to 18.2 flip image color

--- epi_judge_python/matrix_connected_regions.py
+++ epi_judge_python/matrix_connected_regions.py
@@ -2,10 +2,20 @@ from typing import List
 
 from test_framework import generic_test
 
+ADJACENT = [(1,0), (-1,0), (0,1), (0,-1)]
 
+# 18.2, O(V + E) time, O(V) space ?
 def flip_color(x: int, y: int, image: List[List[bool]]) -> None:
-    # TODO - you fill in here.
-    return
+    def dfs(x,y):
+        image[x][y] = not color
+        for dx, dy in ADJACENT:
+            if 0 <= x + dx < len(image) and 0 <= y + dy < len(image[0]) \
+                and image[x+dx][y+dy] is color:
+                dfs(x+dx, y+dy)
+
+    color = image[x][y]
+    dfs(x,y)
+
 
 
 def flip_color_wrapper(x, y, image):
