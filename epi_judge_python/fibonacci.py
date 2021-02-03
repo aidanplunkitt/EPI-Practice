775b69511a45854d9fed8420f7c97a590b93dc9b
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2021-02-03 13:19:32 -0500

    16.0

--- epi_judge_python/fibonacci.py
+++ epi_judge_python/fibonacci.py
@@ -2,8 +2,14 @@ from test_framework import generic_test
 
 
 def fibonacci(n: int) -> int:
-    # TODO - you fill in here.
-    return -1
+    if n < 2:
+        return n
+
+    curr, last = 1, 0
+    for _ in range(n - 1):
+        curr, last = curr + last, curr
+
+    return curr
 
 
 if __name__ == '__main__':
