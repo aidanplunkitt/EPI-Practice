9cdba02d06564d5816652ecf25c087c83cbf8d05
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2021-02-03 15:06:56 -0500

    16.2 recursive

--- epi_judge_python/levenshtein_distance.py
+++ epi_judge_python/levenshtein_distance.py
@@ -2,8 +2,25 @@ from test_framework import generic_test
 
 
 def levenshtein_distance(A: str, B: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    def get_distance(x, y):
+        if dp[x][y] < 0:
+            if x == 0:
+                return y
+            if y == 0:
+                return x
+
+            # if letters are equal, continue to submatrix without adding
+            if A[x-1] == B[y-1]:
+                dp[x][y] = get_distance(x-1, y-1)
+            else:  # get min of add/delete/edit
+                dp[x][y] = min(get_distance(
+                    x-1, y), get_distance(x, y-1), get_distance(x-1, y-1)) + 1
+
+        return dp[x][y]
+
+    dp = [[-1 for _ in range(len(B) + 1)] for _ in range(len(A) + 1)]
+    val = get_distance(len(A), len(B))
+    return val
 
 
 if __name__ == '__main__':
